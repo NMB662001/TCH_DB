60d8cf5b75eba8ab9cf6c07293e83ddf0389be90
NMB661 <nishabhujbal17@gmail.com>
2024-03-11 20:43:42 +0530

    db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { executeQuery } from "./database.js";
 const app = express();
- 
+
 const port = 3000;
 
 app.get('/', async (req, res) => {
@@ -10,7 +10,7 @@ app.get('/', async (req, res) => {
 
 app.get("/getdata", async (req, res) => {
   try {
-    const response = await executeQuery(`SELECT * FROM users `);
+    const response = await executeQuery(`SELECT * FROM cars `);
     console.log(response);
     res.send(response);
   } catch (error) {
@@ -19,53 +19,42 @@ app.get("/getdata", async (req, res) => {
   }
 });
 app.get("/deldata", async (req, res) => {
-    try {
-      const response = await executeQuery(`delete FROM users where id='2af36a23-8cad-4046-b675-9bea4b96e572'`);
-      console.log(response);
-      res.send(response);
-    } catch (error) {
-      console.error('Error executing query:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  app.get("/insertdata", async (req, res) => {
-    try {
-      const response = await executeQuery(`insert into users (name,brand) values('nita','lmn'),('rupali','pqr'),('shruti','def'),('krushna','jkl');`);
-      console.log(response);
-      res.send(response);
-    } catch (error) {
-      console.error('Error executing query:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+  try {
+    const response = await executeQuery(`delete FROM users where id='d32fb7ad-f014-4a93-9b79-3880e45f813e'`);
+    console.log(response);
+    res.send(response);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+// app.get("/insertdata", async (req, res) => {
+//   try {
+//     const response = await executeQuery(`insert into users (name,brand) values('nita','lmn'),('rupali','pqr'),('shruti','def'),('krushna','jkl');`);
+//     console.log(response);
+//     res.send(response);
+//   } catch (error) {
+//     console.error('Error executing query:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
+app.get("/update",async(req,res)=>{
+  try{
+     const response = await executeQuery('update users set name= "banti" where id="360d4659-74a3-4ddd-948e-e871457b2676"');
+  
+   console .log(response);
+    res.send(response);
+  }
+  catch(error){
+console.error('error executing query:',error);
+res.status(500).send('internal server error');
+  }
+});
 
 app.listen(3000, () => {
   console.log(`Server is listening  check at http://localhost:${port}`);
 });
-// import express from 'express'
-
-// const app=express();
-// const port=3000;
-// app.get('/',async(req,res)=>{
-// res.send('express')
-// })
-// app.listen(port,()=>{
-//     console.log('server is working')
-// });
 
 
 
-// import express from "express"
- 
-// const app = express();
- 
-// const port = 3000;
- 
-// (express()).get('/', async(req, res)=>{
-//     res.send("Hii! Server is Up and working")
-// })
- 
- 
-// (express()).listen(3000, () => {
-//     console.log(`Server is listening  check at http://localhost:${3000}`);
-//   });
+
